b6c65724bfd09860b5e0d7c4481edd4ca6e114d2
Omar Meharab <omarmeharab81@gmail.com>
2021-03-21 22:37:02 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, { createContext, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,6 +11,9 @@ import NoMatch from './components/NoMatch/NoMatch';
 import Contact from './components/Contact/Contact';
 import Destination from './components/Destination/Destination';
 import Login from './components/Login/Login';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
 
 function App() {
   const backgroundImage = {
@@ -19,48 +22,56 @@ function App() {
     backgroundPosition: 'center',
     backgroundSize: 'cover'
   };
+
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <Router>
-      <div style={backgroundImage} className="nav-line">
-        <nav>
-        <h1 className="text">Fast Travel</h1>
-          <ul>
-            <li>
-              <Link to="/"><button className="btn btn-primary">Home</button></Link>
-            </li>
-            <li>
-              <Link to="/destination"><button className="btn btn-primary">Destination</button></Link>
-            </li>
-            <li>
-              <Link to="/contact"><button className="btn btn-primary">Contact</button></Link>
-            </li>
-            <li>
-              <Link to="/login"><button className="btn btn-primary" to="/login">Log In</button></Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/destination">
-            <Destination />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/destination/:name">
-            <Destination />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <div style={backgroundImage} className="nav-line">
+          <nav>
+            <h1 className="text">Fast Travel</h1>
+            <ul>
+              <li>
+                <Link to="/"><button className="btn btn-primary">Home</button></Link>
+              </li>
+              <li>
+                <Link to="/destination"><button className="btn btn-primary">Destination</button></Link>
+              </li>
+              <li>
+                <Link to="/contact"><button className="btn btn-primary">Contact</button></Link>
+              </li>
+              <li>
+                <Link to="/login"><button className="btn btn-primary" to="/login">Log In</button></Link>
+              </li>
+              <li>
+                <button className="btn btn-primary user-name" to="/login">{loggedInUser.name}</button>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* <PrivateRoute path="/destination">
+              <Destination />
+            </PrivateRoute> */}
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute exact path="/destination/:name">
+              <Destination />
+            </PrivateRoute>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,50 +1,114 @@
 import './Login.css';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+    const [newUser, setNewUser] = useState(true);
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+
     if (firebase.apps.length === 0) {
         firebase.initializeApp(firebaseConfig);
     }
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
-    const [user, setUser] = useState({});
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const onSubmit = (user) => {
+        if (newUser && user.email && user.password) {
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    const name = user.name;
+                    const { email } = res.user;
+                    const newUserInfo = { email, name };
+                    setLoggedInUser(newUserInfo);
+                    history.replace(from);
+                    setUser(newUserInfo);
+                    updateUserName(name);
+                })
+                .catch(error => {
+                    const newUserInfo = {}
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                });
+        }
+
+        if (!newUser && user.email && user.password) {
+            console.log(newUser);
+            firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    const newUserInfo = {...user};
+                    setUser(newUserInfo);
+                    setLoggedInUser(newUserInfo);
+                    history.replace(from);
+                })
+                .catch(error => {
+                    const newUserInfo = {}
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                });
+        }
+    }
+
+
     const handleGoogleSignIn = () => {
-        firebase.auth()
-            .signInWithPopup(provider)
-            .then((result) => {
-                var credential = result.credential;
-                var token = credential.accessToken;
-                var user = result.user;
-                console.log(user, token);
-                setUser(user);
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(res => {
+                const { displayName, email } = res.user;
+                const newUserInfo = { name: displayName, email };
+                setLoggedInUser(newUserInfo);
+                history.replace(from);
+                setUser(newUserInfo);
             }).catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                var email = error.email;
-                var credential = error.credential;
-                console.log(errorMessage, errorCode, credential, email);
+                const newUserInfo = {}
+                newUserInfo.error = error.message;
+                newUserInfo.success = false;
+                setUser(newUserInfo)
             });
     }
 
+    const updateUserName = name => {
+        const user = firebase.auth().currentUser;
+    
+        user.updateProfile({
+            displayName: name,
+        }).then(function () {
+    
+        }).catch(function (error) {
+    
+        });
+    }
+
     return (
-        <div className = "login-div">
-            <form className="login-form"  onSubmit={handleSubmit(onSubmit)}>
-                <input placeholder="Name" name="firstName" ref={register({ required: true, maxLength: 20 })} />
+        <div className="login-div">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                {newUser && <input placeholder="Name" name="name" ref={register({ required: true, maxLength: 20 })} />}
                 <br />
-                <input placeholder="Email" name="email" type="email" ref={register({ min: 18, max: 99 })} />
+                <input placeholder="Email" name="email" type="email" ref={register({ required: true, min: 18, max: 99 })} />
                 <br />
-                <input placeholder="Password" type="password" name="password" ref={register({ min: 18, max: 99 })} />
-                <br/>
-                <input placeholder="Confirm Password" type="password" name="confirmPassword" ref={register({ min: 18, max: 99 })} />
+                <input placeholder="Password" type="password" name="password" ref={register({ required: true, min: 18, max: 99 })} />
                 <br />
-                <input className="btn btn-primary" type="submit" />
+                {newUser && <input placeholder="Confirm Password" type="password" name="confirmPassword" ref={register({ required: true, min: 18, max: 99 })} />}
+                <br />
+                <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+                <label htmlFor="newUser">New User Sign UP</label>
+                <br />
+                <input className="btn btn-primary" type="submit" value={newUser ? 'Sign Up' : 'Sign In'} />
                 <p>or</p>
                 <button className="btn btn-primary" onClick={handleGoogleSignIn}><FontAwesomeIcon icon={faGoogle} /> Sign In</button>
                 <p>{user.email}</p>
